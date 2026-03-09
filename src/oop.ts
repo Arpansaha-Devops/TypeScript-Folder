38f94e650c1cc729eca2dc23736a4cd8a74af772
Arpan <arpansaha0707@gmail.com>
2026-03-09 23:13:01 +0530

    private Modifier learn

--- src/oop.ts
+++ src/oop.ts
@@ -22,19 +22,58 @@
 
 // Public Access Modifier ----
 
-class Employee {
-  constructor(public name: string, public age: number) {
-    this.age = age;
+// class Employee {
+//   constructor(public name: string, public age: number) {
+//     this.age = age;
+//     this.name = name;
+//   }
+
+//   printName(): void {
+//     console.log(`My name is ${this.name} and my age is ${this.age}`);
+//   }
+// }
+
+// class Manager extends Employee {  // Manager class is inheriting the properties and methods of Employee class
+//   constructor(name: string, age: number, public department: string) {
+//     super(name, age);
+//     this.department = department;
+//   }
+
+//   printDepartment(): void {
+//     console.log(`I am a manager of ${this.department} department`);
+//   }
+// }
+
+// const manager = new Manager("Alice", 30, "Engineering");
+
+// console.log(manager === Employee.prototype); // false, manager is an instance of Manager, not Employee
+// console.log(manager instanceof Employee); // true, manager is an instance of Employee through inheritance
+
+// console.log(manager.name);
+// manager.printName();
+// manager.printDepartment();
+
+
+
+
+// Private Access Modifier ----
+
+class Employee1 {
+  constructor(private name : string, private age : number){
     this.name = name;
+    this.age = age;
   }
 
   printName(): void {
     console.log(`My name is ${this.name} and my age is ${this.age}`);
   }
+
+
 }
 
-class Manager extends Employee {
-  constructor(name: string, age: number, public department: string) {
+
+class Manegaer1 extends Employee1 {
+  constructor(name: string, age: number, private department: string) {
     super(name, age);
     this.department = department;
   }
@@ -44,14 +83,12 @@ class Manager extends Employee {
   }
 }
 
-const manager = new Manager("Alice", 30, "Engineering");
 
-console.log(manager === Employee.prototype); // false, manager is an instance of Manager, not Employee
-console.log(manager instanceof Employee); // true, manager is an instance of Employee through inheritance
-
-console.log(manager.name);
-manager.printName();
-manager.printDepartment();
+const employee = new Employee1("Bob", 25);
+// console.log(employee.name); // Error: Property 'name' is private and only accessible within class 'Employee1'.
+// employee.printName(); // This will work because printName is a method of the Employee1 class and it can access the private properties of the class.
 
 
-
+const manager = new Manegaer1("Alice", 30, "Engineering");
+manager.printDepartment(); // This will work because printDepartment
+manager.printName() // This will work because printName is a method of the Employee1 class and it can access the private properties of the class through inheritance.By super keyword we are calling the constructor of the parent class and passing the name and age to it.
